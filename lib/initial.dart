0bcc89fbf817c5ef23342bc9b97459beb31290f5
elder lira <elderfranca@hotmail.com>
2025-05-22 16:56:24 -0300

    Inserted new information

--- lib/initial.dart
+++ lib/initial.dart
@@ -11,6 +11,44 @@ class Initial extends StatefulWidget {
 class _InitialPage extends State<Initial> {
   @override
   Widget build(BuildContext context) {
-    return Menu(body: Text('tela inicial'));
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final Color backgroundColor = Colors.blueAccent;
+    late String user = 'Renan';
+
+    return Menu(
+      body: Column(
+        children: [
+          SizedBox(height: 30),
+          Container(
+            height: 100,
+            width: screenWidth,
+            decoration: BoxDecoration(color: backgroundColor),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Bem-vindo, $user',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      Text(
+                        'Gerencie seu condomínio de forma inteligente',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text('tela inicial'),
+        ],
+      ),
+    );
   }
 }
